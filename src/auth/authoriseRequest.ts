b8274f2ca1e756ea1f3cffd4a1f70ff73d61b515
Darren Baines <dbaines1@hotmail.com>
2019-03-05 09:32:14 +0000

    Anonymous Calls working

--- src/auth/authoriseRequest.ts
+++ src/auth/authoriseRequest.ts
@@ -1,40 +1,35 @@
-import { Router, Request, Response, NextFunction } from "express";
-import { BasicUser } from "@ngscaffolding/models";
+import { Router, Request, Response, NextFunction } from 'express';
+import { BasicUser } from '@ngscaffolding/models';
 var jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
 export default async function authoriseRequest(req: Request, res: Response, next: NextFunction) {
-    // Get token from request
-    let apiToken = req.headers['authorization'];
+  // Get token from request
+  let apiToken = req.headers['authorization'];
 
-    if(!apiToken){ 
-        req['userDetails'] = null;
-        res.sendStatus(401);
-        return;
-     }
-
-     // Remove Bearer bit
-    apiToken = apiToken.replace('Bearer ','');
+  if (apiToken) {
+    // Remove Bearer bit
+    apiToken = apiToken.replace('Bearer ', '');
 
     // Validate & Decode Token
-    try{
-        let jwtDetails = jwt.verify(apiToken, process.env.JWT_PUBLIC_KEY);
-  
-        // Setup UserDetailsModel from Token Details
-        let userDetails: BasicUser = {...jwtDetails};
-        userDetails.userId = jwtDetails['sub'];
+    try {
+      let jwtDetails = jwt.verify(apiToken, process.env.JWT_PUBLIC_KEY);
 
-        // Add UserDetailsModel to the Request Object
-        req['userDetails'] = userDetails;
-    
+      // Setup UserDetailsModel from Token Details
+      let userDetails: BasicUser = { ...jwtDetails };
+      userDetails.userId = jwtDetails['sub'];
+
+      // Add UserDetailsModel to the Request Object
+      req['userDetails'] = userDetails;
+    } catch (err) {
+      req['userDetails'] = null;
+      // res.sendStatus(401);
+      // return;
     }
-    catch (err) {
-        req['userDetails'] = null;
-        res.sendStatus(401);
-        return;
-    }
-        
-    next();
+  } else {
+    req['userDetails'] = null;
+  }
+
+  next();
 }
-
