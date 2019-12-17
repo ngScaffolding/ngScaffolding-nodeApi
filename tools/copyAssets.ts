6c7dffdb56dcf0d8cfc3602ed9596cb771a12e5e
Darren Baines <darren.baines@netwealth.com>
2019-12-17 10:10:06 +0000

    Fix duplicate web.config

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -2,8 +2,8 @@ import * as shell from "shelljs";
 
 // Copy all the view templates
 shell.cp( "-R", "src/views", "dist/" );
-shell.cp( "-R", "src/web.config", "." );
+shell.cp( "-R", "web.config", "dist/" );
 
 // Copy all the view templates
 shell.cp( "-R", "src/views", "build/" );
-shell.cp( "-R", "src/web.config", "build/" );
+shell.cp( "-R", "web.config", "build/" );
