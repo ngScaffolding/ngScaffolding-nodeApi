7b99fc7afdc2847f3e79d890b7ea687ed5bfdba4
Darren Baines <darren.baines@netwealth.com>
2019-12-17 09:33:39 +0000

    Webpack updates

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -3,3 +3,7 @@ import * as shell from "shelljs";
 // Copy all the view templates
 shell.cp( "-R", "src/views", "dist/" );
 shell.cp( "-R", "src/web.config", "." );
+
+// Copy all the view templates
+shell.cp( "-R", "src/views", "build/" );
+shell.cp( "-R", "src/web.config", "build/" );
