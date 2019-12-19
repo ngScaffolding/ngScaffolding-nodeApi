ac2b1f11248d8ff086476fed169c8c06c07906a3
Darren Baines <darren.baines@netwealth.com>
2019-12-19 08:14:36 +0000

    Build Date

--- src/App.ts
+++ src/App.ts
@@ -8,7 +8,8 @@ import { initialSetup } from './initialSetup/setup';
 var winston = require('./config/winston');
 
 var jwt = require('express-jwt');
-var cors = require('cors')
+var cors = require('cors');
+const fs = require('fs');
 
 import { RouterSetup } from './routes/routing';
 
@@ -17,43 +18,53 @@ var argv = require('minimist')(process.argv.slice(2));
 
 // Creates and configures an ExpressJS web server.
 class App {
-  
-  // ref to Express instance
-  public express: express.Application;
+    // ref to Express instance
+    public express: express.Application;
 
-  //Run configuration methods on the Express instance.
-  constructor() {
-    if (argv['setup']) {
-      winston.info('Running Initial setup');
-      initialSetup();
+    //Run configuration methods on the Express instance.
+    constructor() {
+        if (argv['setup']) {
+            winston.info('Running Initial setup');
+            initialSetup();
+        }
+
+        winston.info('API Starting');
+
+        this.express = express();
+        this.middleware();
+
+        let router = new RouterSetup(this.express);
+        router.configure();
+
+        winston.info(`__dirname: ${__dirname}`);
+
+        // Add builddate
+        const pathIndex = 'index.js';
+        var buildDate = 'Debug Mode'
+        if (fs.existsSync(pathIndex)) {
+            const { mtime } = fs.statSync(pathIndex);
+            buildDate = mtime;
+        }
+        winston.info(`Build Date: ${buildDate}`);
+        this.express.locals.buildDate = buildDate;
     }
 
-    winston.info('API Starting');
-    
-    this.express = express();
-    this.middleware();
-
-   let router = new RouterSetup(this.express);
-   router.configure();
-  }
-
-  // Configure Express middleware.
-  private middleware(): void {
-
-    this.express.set('views', path.join(__dirname, 'views'));
-    this.express.set('view engine', 'pug');
-    this.express.use(morgan('combined', { stream: winston.stream }));
-    this.express.use(bodyParser.json({limit: '10mb'}));
-    this.express.use(cors())
-    this.express.use(authoriseRequest);
-    this.express.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
-    this.express.use(function(err,req,res,next){
-      if(err) {
-        winston.error(err);
-        console.error(err);
-      }
-    });
-  }
+    // Configure Express middleware.
+    private middleware(): void {
+        this.express.set('views', 'views');
+        this.express.set('view engine', 'pug');
+        this.express.use(morgan('combined', { stream: winston.stream }));
+        this.express.use(bodyParser.json({ limit: '10mb' }));
+        this.express.use(cors());
+        this.express.use(authoriseRequest);
+        this.express.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+        this.express.use(function(err, req, res, next) {
+            if (err) {
+                winston.error(err);
+                console.error(err);
+            }
+        });
+    }
 }
 
 export default new App().express;
