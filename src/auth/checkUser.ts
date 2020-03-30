75d37cd2269653415d9fae310a114c382ab91a91
Darren Baines <dbaines1@hotmail.com>
2020-03-30 18:00:05 +0100

    Menu ownership

--- src/auth/checkUser.ts
+++ src/auth/checkUser.ts
@@ -1,28 +1,33 @@
-import { BasicUser, CoreMenuItem } from "../models/index";
+import { BasicUser, CoreMenuItem, AppSettings } from '../models/index';
+import { getSetting } from '../utils/appSettings.helper';
 
 export function checkUser(user: BasicUser, menu: CoreMenuItem): boolean {
+    
+    // For owned menu items only owner is allowed
+    if (menu.ownerId) {
+        const Uniquefield = getSetting(AppSettings.authUserUniqueField, 'userId');
+        return user[Uniquefield] === menu.ownerId;
+    }
 
     // This means that items with no allowedRoles - Everyone gets to play
-    if(!menu.roles) return true;
-
-      
+    if (!menu.roles) return true;
 
     var checkUserRoles = [...user.role];
     var checkAllowedRoles = [];
 
-    if(Array.isArray(menu.roles)) {
-      checkAllowedRoles = [...menu.roles];
+    if (Array.isArray(menu.roles)) {
+        checkAllowedRoles = [...menu.roles];
     } else {
-      checkAllowedRoles = [menu.roles];
+        checkAllowedRoles = [menu.roles];
     }
 
     var isUserCool = false;
     checkUserRoles.forEach(checkUserRole => {
         checkAllowedRoles.forEach(checkAllowedRole => {
-            if(checkUserRole === checkAllowedRole){
+            if (checkUserRole === checkAllowedRole) {
                 isUserCool = true;
             }
-        })
+        });
     });
 
     if (menu.userIds) {
@@ -34,5 +39,4 @@ export function checkUser(user: BasicUser, menu: CoreMenuItem): boolean {
     }
 
     return isUserCool;
-
 }
